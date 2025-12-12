f63067f03502c620df350864e022cb9385f0e97a
Shameer <54391497+Shameer29@users.noreply.github.com>
2025-12-12 17:09:15 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['three'],
+  },
+  build: {
+    commonjsOptions: {
+      exclude: ['three'],
+    },
+    rollupOptions: {
+      external: [],
+    },
+    minify: "esbuild",
+    terserOptions: {
+      mangle: false,
+      compress: false,
+    }
+  }
 })
